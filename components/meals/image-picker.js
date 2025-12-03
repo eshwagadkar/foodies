e7e52830afb81e9e10da55c01af5d1d329f0aaa6
eshwagadkar <eshwagadkar007@gmail.com>
2025-12-03 14:07:26 +0530

    improve the image picker component

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -14,7 +14,10 @@ export default function ImagePicker({label, name}) {
     function handleImageChange(event) {
         const file = event.target.files[0]
 
-        if(!file){return}
+        if(!file){
+            setPickedImage(null)
+            return
+        }
     
         const fileReader = new FileReader()
 
@@ -40,6 +43,7 @@ export default function ImagePicker({label, name}) {
                 name={name}
                 ref={imageInput}
                 onChange={handleImageChange}
+                required
             />
             <button 
                 className={classes.button} 
